bc9056bd4ea519b28e5bb90427a5f7b6f370be5a
Abdullah al Sakib <abdullahalsakib903@gmail.com>
2021-06-25 16:35:12 +0600

    customized most of it exept blogs😁

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,80 +1,92 @@
-import React from 'react';
-import styled from 'styled-components';
-import {InnerLayout} from '../styles/Layouts';
-import Title from '../Components/Title';
-import SmallTitle from '../Components/SmallTitle';
-import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
-import SchoolIcon from '@material-ui/icons/School';
-import ResumeItem from '../Components/ResumeItem';
+import React from "react";
+import styled from "styled-components";
+import { InnerLayout } from "../styles/Layouts";
+import Title from "../Components/Title";
+import SmallTitle from "../Components/SmallTitle";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import SchoolIcon from "@material-ui/icons/School";
+import ResumeItem from "../Components/ResumeItem";
 
 function Resume() {
-    const briefcase = <BusinessCenterIcon />
-    const school = <SchoolIcon />
-    return (
-        <ResumeStyled>
-            <Title title={'Resume'} span={'resume'} />
-            <InnerLayout>
-                <div className="small-title">
-                    <SmallTitle icon={briefcase} title={'Working Experience'} />
-                </div>
-                <div className="resume-content">
-                    <ResumeItem 
-                        year={'2015 - 2020'} 
-                        title={'Computer Science Teacher'}
-                        subTitle={'Sussex University'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
-                    />
-                    <ResumeItem 
-                        year={'2015 - Present'} 
-                        title={'Full Stack Developer'}
-                        subTitle={'Microsoft Studios'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
-                    />
-                    <ResumeItem 
-                        year={'2010 - 2017'} 
-                        title={'User Interface Designer'}
-                        subTitle={'Google Inc'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
-                    />
-                </div>
-                <div className="small-title u-small-title-margin">
-                    <SmallTitle icon={school} title={'Educational Qualifications'} />
-                </div>
-                <div className="resume-content ">
-                    <ResumeItem 
-                        year={'201 - 2022'} 
-                        title={'Computer Science Degree'}
-                        subTitle={'Sussex University'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
-                    />
-                    <ResumeItem 
-                        year={'2015 - 2017'} 
-                        title={'A - Levels'}
-                        subTitle={'Church Hill High'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
-                    />
-                    <ResumeItem 
-                        year={'2015 - 2017'} 
-                        title={'High School Graduation'}
-                        subTitle={'ABC School'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
-                    />
-                </div>
-            </InnerLayout>
-        </ResumeStyled>    
-    )
+  const briefcase = <BusinessCenterIcon />;
+  const school = <SchoolIcon />;
+  return (
+    <ResumeStyled>
+      <Title title={"Resume"} span={"resume"} />
+      <InnerLayout>
+        <div className="small-title">
+          <SmallTitle icon={briefcase} title={"Working Experience"} />
+        </div>
+        <div className="resume-content">
+          <ResumeItem
+            year={"2020 - present"}
+            title={"Freelancer"}
+            subTitle={"Upwork"}
+            text={
+              "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. "
+            }
+          />
+          <ResumeItem
+            year={"2015 - Present"}
+            title={"Full Stack Developer"}
+            subTitle={"Self-taught"}
+            text={
+              "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. "
+            }
+          />
+          <ResumeItem
+            year={"2020 - present"}
+            title={"DataBase Designer"}
+            subTitle={"Self-taught"}
+            text={
+              "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. "
+            }
+          />
+        </div>
+        <div className="small-title u-small-title-margin">
+          <SmallTitle icon={school} title={"Educational Qualifications"} />
+        </div>
+        <div className="resume-content ">
+          <ResumeItem
+            year={"2020 - 2024"}
+            title={"Diploma in Computer Science"}
+            subTitle={"Dhaka Polytechnic Institute"}
+            text={
+              "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. "
+            }
+          />
+          <ResumeItem
+            year={"2018 - 2020"}
+            title={"High School"}
+            subTitle={"German technical training center"}
+            text={
+              "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. "
+            }
+          />
+          <ResumeItem
+            year={"2015 - 2017"}
+            title={"Primary School Graduation"}
+            subTitle={"Savar Cantonment Public School and College"}
+            text={
+              "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. "
+            }
+          />
+        </div>
+      </InnerLayout>
+    </ResumeStyled>
+  );
 }
 
 const ResumeStyled = styled.section`
-    .small-title{
-        padding-bottom: 3rem;
-    }
-    .u-small-title-margin{
-        margin-top: 4rem;
-    }
+  .small-title {
+    padding-bottom: 3rem;
+  }
+  .u-small-title-margin {
+    margin-top: 4rem;
+  }
 
-    .resume-content{
-        border-left: 2px solid var(--border-color);
-    }
+  .resume-content {
+    border-left: 2px solid var(--border-color);
+  }
 `;
-export default Resume
+export default Resume;
--- src/Components/ServicesSection.js
+++ src/Components/ServicesSection.js
@@ -1,58 +1,63 @@
-import React from 'react';
-import styled from 'styled-components';
-import {InnerLayout} from '../styles/Layouts';
-import Title from '../Components/Title';
-import ServiceCard from '../Components/ServiceCard';
-import design from '../img/design.svg';
-import intelligence from '../img/intelligence.svg';
-import gamedev from '../img/game-dev.svg';
+import React from "react";
+import styled from "styled-components";
+import { InnerLayout } from "../styles/Layouts";
+import Title from "../Components/Title";
+import ServiceCard from "../Components/ServiceCard";
+import serverStorage from "../img/server-storage.svg";
+import backend from "../img/backend.svg";
+import webDesign from "../img/web-design.svg";
 
 function ServicesSection() {
-    return (
-        <InnerLayout>
-            <ServicesSectionStyled>
-                <Title title={'Services'} span={'services'} />
-                <div className="services">
-                    <ServiceCard 
-                        image={design} 
-                        title={'Web Design'} 
-                        paragraph={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'}
-                    />
-                    <div className="mid-card">
-                        <ServiceCard 
-                            image={intelligence} 
-                            title={'Artificial Intelligence'} 
-                            paragraph={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'}
-                        />
-                    </div>
-                    <ServiceCard 
-                        image={gamedev} 
-                        title={'Game Development'} 
-                        paragraph={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur ratione quod.'}
-                    />
-                </div>
-            </ServicesSectionStyled>
-        </InnerLayout>
-    )
+  return (
+    <InnerLayout>
+      <ServicesSectionStyled>
+        <Title title={"Services"} span={"services"} />
+        <div className="services">
+          <ServiceCard
+            image={webDesign}
+            title={"Front-End Developer"}
+            paragraph={
+              "Over 2.5+ years of IT experience which includes 2+ years of Extensive experience as a React JS Developer . Experienced in React JS and working with React Flux architecture. Worked on responsive design and developed a single ISOMORPHIC responsive website that could be served to desktop, Tablets and mobile users using Reactjs. Experienced in working with Redux architecture using complex Object-Oriented concepts in improving the performance of the website. Good Experience in React.js for creating interactive UI's using One-way data flow, Virtual DOM, JSX, React Native concepts. Familiar with creating Custom Reusable React Components Library."
+            }
+          />
+          <div className="mid-card">
+            <ServiceCard
+              image={backend}
+              title={"Back-End Developer"}
+              paragraph={
+                "Experienced in MERN stack development Mongo dB, Express, Node, and React.Strong in using the Node.js, and Require.js and MVC Frameworks in the development of the web application. Expertise in RESTful, GraphQL to integrate between Application to Application"
+              }
+            />
+          </div>
+          <ServiceCard
+            image={serverStorage}
+            title={"Database Designer"}
+            paragraph={
+              "Expertise in writing SQL, MySQL, PostgreSQL and MongoDB"
+            }
+          />
+        </div>
+      </ServicesSectionStyled>
+    </InnerLayout>
+  );
 }
 
 const ServicesSectionStyled = styled.section`
-    .services{
-        margin-top: 5rem;
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-gap: 1.5rem;
-        @media screen and (max-width:1000px){
-            flex-direction: column;
-        }
-        @media screen and (max-width:950px){
-            grid-template-columns: repeat(2, 1fr);
-        }
-        @media screen and (max-width:650px){
-            grid-template-columns: repeat(1, 1fr);
-        }
-       
+  .services {
+    margin-top: 5rem;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 1.5rem;
+    @media screen and (max-width: 1000px) {
+      flex-direction: column;
     }
+    @media screen and (max-width: 950px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+    @media screen and (max-width: 650px) {
+      grid-template-columns: repeat(1, 1fr);
+    }
+  }
 `;
 
 export default ServicesSection;
